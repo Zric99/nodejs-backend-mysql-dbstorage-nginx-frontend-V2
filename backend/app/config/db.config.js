0ac9d1d3509c6a98f1ef68dbb5a23dfb5da79db4
Zric99 <65248246+Zric99@users.noreply.github.com>
2022-01-23 13:33:17 +0100

    Update db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST:"localhost",
+  HOST: process.env.DB_HOST || "localhost",
   USER: process.env.DB_USER || "root",
   PASSWORD: process.env.DB_PASSWORD || "123456",
   DB: process.env.DB_NAME || "testdb",
